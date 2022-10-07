de0f494ae83441797f7871236d4f59e2baf83a60
Vladyslav Patoka <sanderkloj@gmail.com>
2022-10-07 16:48:52 +0300

    Adding Loger for logs

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -1,27 +1,42 @@
 from distutils.spawn import find_executable
+from loguru import logger
 
 import pytest
 from framework.locators import StartPageLocators, LoginPageLocators, MainPageLocators
 from tests.conftest import driver
-
+logger.add("debug_test_login.log", format="{time} {level} {message}", level="DEBUG")
 
 def test_user_login(user_login_fixture):
+    logger.debug("Starting test_user_login")
     page = user_login_fixture
     page.go_to_log_in_page()
+    logger.debug("Tap on Log In button")
     page.wait()
     page.write_password_in_field("qa_automation_password")
+    logger.debug("Writing password in login field")
     page.write_mail_in_field("qa.ajax.app.automation@gmail.com")
+    logger.debug("Writing mail in login field")
     page.click_log_in_button()
+    logger.debug("Tap on Log In button")
     page.should_be_main_page()
+    logger.debug("Checking is that page wich need")
+    logger.debug("Finish test_user_login\n")
 
 @pytest.mark.parametrize('password, mail', [("qa_automation_password", ""),
                                             ("", "qa.ajax.app.automation@gmail.com"),
                                             ("password", "test@gmail.com")])
 def test_user_not_login(user_login_fixture, password, mail):
+    logger.debug("Starting test_user_not_login")
     page = user_login_fixture
     page.go_to_log_in_page()
+    logger.debug("Tap on Log In button")
     page.wait()
     page.write_password_in_field(password)
+    logger.debug("Writing password in login field")
     page.write_mail_in_field(mail)
+    logger.debug("Writing mail in login field")
     page.click_log_in_button()
+    logger.debug("Tap on Log In button")
     page.should_not_be_main_page()
+    logger.debug("Checking is user not login")
+    logger.debug("Finish test_user_not_login\n")
--- tests/sidebar/test_sidebar.py
+++ tests/sidebar/test_sidebar.py
@@ -1,53 +1,104 @@
 from distutils.spawn import find_executable
+from re import fullmatch
+from loguru import logger
 
 import pytest
 from tests.conftest import driver
+logger.add("debug_test_sidebar.log", format="{time} {level} {message}", level="DEBUG")
 
+@logger.catch
 def test_sidebar_hub_item(user_sidebar_fixture):
+    logger.debug("Starting test_sidebar_hub_item")
     page = user_sidebar_fixture
     page.go_to_log_in_page()
+    logger.debug("Tap on Log In button")
     page.wait()
     page.write_password_in_field("qa_automation_password")
+    logger.debug("Writing password in login field")
     page.write_mail_in_field("qa.ajax.app.automation@gmail.com")
+    logger.debug("Writing mail in login field")
     page.click_log_in_button()
+    logger.debug("Tap on Log In button")
     page.click_later_button()
+    logger.debug("Tap on Later button")
     page.click_menu_button()
+    logger.debug("Tap on Menu icon")
     page.click_add_hub_button()
+    logger.debug("Tap on Add Hub button")
+    page.wait()
     page.should_be_hub_page()
-    
+    logger.debug("Checking is that page wich need")
+    logger.debug("Finish test_sidebar_hub_item\n")
+
+@logger.catch   
 def test_sidebar_settings_item(user_sidebar_fixture):
+    logger.debug("Starting test_sidebar_settings_item")
     page = user_sidebar_fixture
     page.go_to_log_in_page()
+    logger.debug("Tap on Log In button")
     page.wait()
     page.write_password_in_field("qa_automation_password")
+    logger.debug("Writing password in login field")
     page.write_mail_in_field("qa.ajax.app.automation@gmail.com")
+    logger.debug("Writing mail in login field")
     page.click_log_in_button()
+    logger.debug("Tap on Log In button")
     page.click_later_button()
+    logger.debug("Tap on Later button")
     page.click_menu_button()
+    logger.debug("Tap on Menu icon")
     page.click_settings_button()
+    logger.debug("Tap on Settings button")
+    page.wait()
     page.should_be_settings_page()
+    logger.debug("Checking is that page wich need")
+    logger.debug("Finish test_sidebar_settings_item\n")
 
+@logger.catch
 def test_sidebar_help_item(user_sidebar_fixture):
+    logger.debug("Starting test_sidebar_settings_item")
     page = user_sidebar_fixture
     page.go_to_log_in_page()
+    logger.debug("Tap on Log In button")
     page.wait()
     page.write_password_in_field("qa_automation_password")
+    logger.debug("Writing password in login field")
     page.write_mail_in_field("qa.ajax.app.automation@gmail.com")
+    logger.debug("Writing mail in login field")
     page.click_log_in_button()
+    logger.debug("Tap on Log In button")
     page.click_later_button()
+    logger.debug("Tap on Later button")
     page.click_menu_button()
+    logger.debug("Tap on Menu icon")
     page.click_help_button()
+    logger.debug("Tap on Help button")
+    page.wait()
     page.should_be_help_page()
+    logger.debug("Checking is that page wich need")
+    logger.debug("Finish test_sidebar_help_item\n")
 
+@logger.catch
 def test_sidebar_logs_item(user_sidebar_fixture):
+    logger.debug("Starting test_sidebar_settings_item")
     page = user_sidebar_fixture
     page.go_to_log_in_page()
+    logger.debug("Tap on Log In button")
     page.wait()
     page.write_password_in_field("qa_automation_password")
+    logger.debug("Writing password in login field")
     page.write_mail_in_field("qa.ajax.app.automation@gmail.com")
+    logger.debug("Writing mail in login field")
     page.click_log_in_button()
+    logger.debug("Tap on Log In button")
     page.click_later_button()
+    logger.debug("Tap on Later button")
     page.click_menu_button()
+    logger.debug("Tap on Menu icon")
     page.click_logs_button()
+    logger.debug("Tap on Logs button")
+    page.wait()
     page.should_be_logs_page()
+    logger.debug("Checking is that page wich need")
+    logger.debug("Finish test_sidebar_logs_item\n")
 
